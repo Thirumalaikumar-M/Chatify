8841cfa8d7ff4cf114b147ae447086c7d5f2054e
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-03-31 09:39:34 +0530

    Implement endpoint to fetch messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -27,8 +27,7 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    await conversation.save();
-    await newMessage.save();
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
@@ -37,6 +36,22 @@ export const sendMessage = async (req, res) => {
   }
 };
 
-export const getMessages = (req, res) => {
-  console.log("messages got");
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
