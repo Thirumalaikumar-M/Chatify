f12d56b6e451c9a80415f64f43c8ac312663cf2c
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-05-13 21:46:46 +0530

    fix : Solve toast pop-up issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           element={authUser ? <Navigate to="/" /> : <Signup />}
         />{" "}
       </Routes>
+      <Toaster />
     </div>
   );
 }
